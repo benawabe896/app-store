3d15ab39e8d78182368281b8dbe3f2084e25f1bc
ben-powers <ben.powers@am.sony.com>
2014-07-08 16:52:02 -0700

    Minor cleanup

--- app/routes/comment.js
+++ app/routes/comment.js
@@ -1,41 +1,55 @@
 var Comment = require('../models/comment');
 
-// Application Comments Endpoints
-router.route('/comments/:comment_id')
-  .get(function(req, res) {
-    Comment.findById(req.params.comment_id, function(err, comment) {
-      if(err) {
-        res.send(err);
-      }
+module.exports = function(router){
+  // Comments
+  router.route('/comments')
+    .get(function(req, res) {
+      Comment.find(function(err, comments){
+        if(err) {
+          res.send(err);
+        }
 
-      res.json(comment);
+        res.json(comments);
+      });
     });
-  })
-  .put(function(req, res) {
-    Comment.findById(req.params.comment_id, function(err, comment){
-      if(err) {
-        res.send(err);
-      }
 
-      comment.description = req.body.description;
-
-      comment.save(function(err) {
+  // Comment
+  router.route('/comments/:comment_id')
+    .get(function(req, res) {
+      Comment.findById(req.params.comment_id, function(err, comment) {
         if(err) {
           res.send(err);
         }
 
         res.json(comment);
       });
-    });
-  })
-  .delete(function(req, res) {
-    Comment.remove({
-      _id: req.params.comment_id
-    }, function(err) {
-      if(err) {
-        res.send(err);
-      }
+    })
+    .put(function(req, res) {
+      Comment.findById(req.params.comment_id, function(err, comment){
+        if(err) {
+          res.send(err);
+        }
 
-      res.json({ message: 'Successfully deleted' });
+        comment.description = req.body.description;
+
+        comment.save(function(err) {
+          if(err) {
+            res.send(err);
+          }
+
+          res.json(comment);
+        });
+      });
+    })
+    .delete(function(req, res) {
+      Comment.remove({
+        _id: req.params.comment_id
+      }, function(err) {
+        if(err) {
+          res.send(err);
+        }
+
+        res.json({ message: 'Successfully deleted' });
+      });
     });
-  });
+}
--- server.js
+++ server.js
@@ -2,15 +2,17 @@ var express    = require('express');
 var app        = express();
 var bodyParser = require('body-parser');
 var busboy = require('connect-busboy');
-router = express.Router();
+var router = express.Router();
 
 var mongoose   = require('mongoose');
 mongoose.connect('mongodb://localhost/app-store');
 
-require('./app/routes/application.js');
-
 app.use(bodyParser());
 app.use(busboy());
+
+// Routes
+require('./app/routes/application.js')(router);
+require('./app/routes/comment.js')(router);
 
 app.use('/api', router);
 
