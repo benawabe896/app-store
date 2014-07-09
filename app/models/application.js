2b096004509d26ce811490cf2f56636ab4e8d117
ben-powers <ben.powers@am.sony.com>
2014-07-08 19:35:16 -0700

    Formatting

--- app/models/application.js
+++ app/models/application.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ApplicationSchema = new Schema({
-	name: String,
+  name: String,
   description: String,
   author: String,
   price: Number
--- app/models/comment.js
+++ app/models/comment.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CommentSchema = new Schema({
-	application_id: String,
+  application_id: String,
   description: String
 });
 
